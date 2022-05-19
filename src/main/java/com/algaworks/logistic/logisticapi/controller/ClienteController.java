d9ee23ce5c03693244620a3c4702feece6f267c7
tiago <tiagoms@ciandt.com>
2022-05-19 16:38:43 -0300

    Usando spring data JPA biblioteca que cria os repositorios que é responsável por implementar os métodos que fazem as operações de persistência de dados

--- src/main/java/com/algaworks/logistic/logisticapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/logistic/logisticapi/controller/ClienteController.java
@@ -1,24 +1,22 @@
 package com.algaworks.logistic.logisticapi.controller;
 
 import com.algaworks.logistic.logisticapi.domain.Cliente;
+import com.algaworks.logistic.logisticapi.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
 
-        return manager.createQuery("from Cliente",Cliente.class)
-                .getResultList();
+        return clienteRepository.findAll();
     }
 }
