5c6b6ed68e403e9ae059cc685d3f9f9333b125e4
tiago <tiagoms@ciandt.com>
2022-05-20 17:02:03 -0300

    Trata mensagem da exception NegocioException adicionando um novo método na classe ApiExceptionHandler

--- src/main/java/com/algaworks/logistic/logisticapi/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/logistic/logisticapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algaworks.logistic.logisticapi.api.exceptionHandler;
 
+import com.algaworks.logistic.logisticapi.domain.exception.NegocioException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,6 +11,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -43,5 +45,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         problem.setCampos(campos);
 
         return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<Object> handleNegocioException(NegocioException ex, WebRequest request){
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Problem problem = new Problem();
+        problem.setStatus(status.value());
+        problem.setDateTime(LocalDateTime.now());
+        problem.setTitulo(ex.getMessage());
+
+        return handleExceptionInternal(ex,problem,new HttpHeaders(),status,request);
     }
 }
