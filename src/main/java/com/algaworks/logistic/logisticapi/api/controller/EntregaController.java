3da683a9bdd5cf11a5858803739f50c2ed49524b
tiago <tiagoms@ciandt.com>
2022-05-25 08:45:53 -0300

    Adiciona as requisições para listar e buscar entregas no controlador de entregas

--- src/main/java/com/algaworks/logistic/logisticapi/api/controller/EntregaController.java
+++ src/main/java/com/algaworks/logistic/logisticapi/api/controller/EntregaController.java
@@ -2,18 +2,22 @@ package com.algaworks.logistic.logisticapi.api.controller;
 
 
 import com.algaworks.logistic.logisticapi.domain.model.Entrega;
+import com.algaworks.logistic.logisticapi.domain.repository.EntregaRepository;
 import com.algaworks.logistic.logisticapi.domain.service.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/entregas")
 public class EntregaController {
 
+    private EntregaRepository entregaRepository;
     private SolicitacaoEntregaService solicitacaoEntregaService;
 
     @PostMapping
@@ -21,5 +25,18 @@ public class EntregaController {
     public Entrega solicitar (@Valid @RequestBody Entrega entrega){
 
         return solicitacaoEntregaService.solicitar(entrega);
+    }
+
+    @GetMapping
+    public List<Entrega> listar(){
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+
+        return entregaRepository.findById(entregaId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
